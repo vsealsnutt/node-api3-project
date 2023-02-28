33e28e3e5d8180bc7d2b9435b8d4852bfc09225c
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-02-28 10:55:08 -0500

    wrote put request for updating a user

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -5,15 +5,12 @@ const {
   validatePost 
 } = require('../middleware/middleware');
 
-// You will need `users-model.js` and `posts-model.js` both
-// The middleware functions also need to be required
 const User = require('./users-model');
 const Post = require('../posts/posts-model');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
   User.get()
     .then(users => {
       res.json(users);
@@ -22,14 +19,10 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
   res.json(req.user);
 });
 
 router.post('/', validateUser, (req, res, next) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
   User.insert({ name: req.name })
     .then(newUser => {
       res.status(201).json(newUser);
@@ -37,10 +30,12 @@ router.post('/', validateUser, (req, res, next) => {
     .catch(next)
 });
 
-router.put('/:id', validateUserId, validateUser, (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
+  User.update(req.params.id, { name: req.name })
+    .then(updatedUser => {
+      res.json(updatedUser);
+    })
+    .catch(next)
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
