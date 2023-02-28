6af567b9b675e8fffdb58bbc2c55a3b47bcacc25
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-02-28 10:36:12 -0500

    wrote the get helper for users

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -7,11 +7,18 @@ const {
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
+const User = require('./users-model');
+const Post = require('../posts/posts-model');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  User.get()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(next)
 });
 
 router.get('/:id', validateUserId, (req, res) => {
@@ -44,6 +51,14 @@ router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+});
+
+router.use((err, req, res, next) => {
+  res.status(err.staus || 500).json({
+    customMessage: 'something went wrong on the users router',
+    message: err.message,
+    stack: err.stack 
+  });
 });
 
 // do not forget to export the router
