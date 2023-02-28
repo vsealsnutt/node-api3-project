5c3000f8f55ae0995fa607f8e187db0b62ef3257
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-02-28 10:38:06 -0500

    wrote the get user request for an individual user

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -24,6 +24,7 @@ router.get('/', (req, res, next) => {
 router.get('/:id', validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.json(req.user);
 });
 
 router.post('/', validateUser, (req, res) => {
