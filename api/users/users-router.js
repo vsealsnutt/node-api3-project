e9d1d2f1ff76afe38bafd3c959d997778229d71a
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-02-28 10:42:32 -0500

    wrote post request for user

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -27,9 +27,14 @@ router.get('/:id', validateUserId, (req, res) => {
   res.json(req.user);
 });
 
-router.post('/', validateUser, (req, res) => {
+router.post('/', validateUser, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  User.insert({ name: req.name })
+    .then(newUser => {
+      res.status(201).json(newUser);
+    })
+    .catch(next)
 });
 
 router.put('/:id', validateUserId, validateUser, (req, res) => {
